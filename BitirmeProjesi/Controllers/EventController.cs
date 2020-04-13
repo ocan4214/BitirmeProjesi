ab5b708d90ba92fc7a777f734178fbf3dfacf480
ocan4214 <hasanonurcansarihan@gmail.com>
2020-04-13 11:30:01 +0300

    event kaydetme

--- BitirmeProjesi/Controllers/EventController.cs
+++ BitirmeProjesi/Controllers/EventController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BitirmeProjesi.Models;
+using BitirmeProjesi.Models.ViewModel;
 
 namespace BitirmeProjesi.Controllers
 {
@@ -11,18 +14,47 @@ namespace BitirmeProjesi.Controllers
     public class EventController : Controller
     {
         [HttpGet]
-        public ActionResult EventCreate()
+        public ActionResult EventCreate(int groupid)
         {
+            ViewBag.groupid = groupid;
             return View();
         }
 
 
         [HttpPost]
-        public ActionResult EventCreate([Bind(Exclude= "EventCreateDate,EventUpdateDate,EventId")]Event eventM,HttpPostedFileBase ThumbnailImage)
+        public ActionResult EventCreate(int groupid,[Bind(Exclude= "EventGroup,EventCreateDate,EventUpdateDate,EventId")]Event viewModel,HttpPostedFileBase ThumbnailImage)
         {
+            LogRegDBEntities1 db = new LogRegDBEntities1();
+            if(ModelState.IsValid)
+            {
+                if(ThumbnailImage!=null && ThumbnailImage.ContentLength>0)
+                {
+                    using (var reader = new BinaryReader(ThumbnailImage.InputStream))
+                    {
+                        viewModel.EventThumbnail = reader.ReadBytes(ThumbnailImage.ContentLength);
+                    }
+                }
+            }
+            viewModel.EventUpdateDate = DateTime.Now;
+            viewModel.EventCreateDate = DateTime.Now;
+            viewModel.EventGroup = groupid;
+            
+            db.Events.Add(viewModel);
 
-
-
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                foreach (var entityValidationErrors in ex.EntityValidationErrors)
+                {
+                    foreach (var validationError in entityValidationErrors.ValidationErrors)
+                    {
+                        Response.Write("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
+                    }
+                }
+            }
 
             return View();
         }
